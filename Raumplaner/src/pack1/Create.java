7a94ab7c42e78acb4dba05572984df17f3232edc
HakunaBatata <bata.kunz@hotmail.de>
2017-06-01 23:18:50 +0200

    Test Commit den raumplaner

---
+++ Raumplaner/src/pack1/Create.java
@@ -0,0 +1,24 @@
+package pack1;
+
+import javax.swing.*;
+
+
+public class Create {
+
+	JFrame start;
+	JButton neu;
+	JButton template;
+	
+	public Create() {
+		
+		start = new JFrame("Skatch Room - New Room");
+		start.setSize(400, 300);
+		start.setLocationRelativeTo(null);
+		start.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		start.setVisible(true);
+		
+		neu = new JButton;
+		
+	}
+
+}
